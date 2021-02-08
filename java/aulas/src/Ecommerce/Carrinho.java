9493b23eabc04234befd0c86d34fefe3e175f8cb
MariaNazar <nazare740@gmail.com>
2021-02-08 17:13:39 -0300

    Ecommerce em POO Java fim de módulo 08/02/2021

---
+++ java/aulas/src/Ecommerce/Carrinho.java
@@ -0,0 +1,35 @@
+package Ecommerce;
+
+public class Carrinho extends Produto{
+	private int qtdCompra;
+
+    public Carrinho(int codigo, String descricao, double preco, int qtdEstoque, int qtdCompra) {
+		super(codigo, descricao, preco, qtdEstoque);
+		this.qtdCompra = qtdCompra;
+	}
+
+	public void subTotal() 
+    {
+    	
+    }
+
+    public Carrinho(int codigo, int qtdCompra) 
+    {
+        super(codigo);
+        this.qtdCompra = qtdCompra;
+        System.out.println("");
+    }
+
+    public int getQtdCompra() 
+    {
+        return qtdCompra;
+    }
+
+    public void setQtdCompra(int qtdCompra) 
+    {
+        this.qtdCompra = qtdCompra;
+    }
+
+	
+
+}
